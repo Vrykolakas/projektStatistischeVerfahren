8f1427456ba2a31c9a7cb5321a954fb832a5cc63
Christian Lengert <clengert@googlemail.com>
2018-08-14 13:13:43 +0200

    add dataset pdf

--- main.R
+++ main.R
@@ -1,2 +1,11 @@
-library(readr)
-read_csv("dataset/crimes.csv") -> data
+read.csv("dataset/crimes.csv") -> data
+
+
+data[colnames(data)!="region" & colnames(data)!="name"] -> numericData
+cov(numericData) -> covNumericData
+
+
+sapply(10:nrow(data),function(x){
+  subset = covNumericData[sample(nrow(data),x),]
+  cov(subset) -> covSubset
+})
