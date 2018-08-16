d0676ab1dc663fb4cba6455dcdee9c6b3b50c59f
Christian Lengert <clengert@googlemail.com>
2018-08-16 18:13:15 +0200

    add Mallows Cp with leaps package add simple model

--- main.R
+++ main.R
@@ -1,11 +1,42 @@
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+library(purrr)
+library(leaps)
+
 read.csv("dataset/crimes.csv") -> data
+colnames(data) -> names
+
+#Wählen Sie ein konkretes (einfaches) wahres Modell:
+c(
+  "crimes", #Zielgröße
+  "wcon", # Wöchentlicher Lohn im Baugewerbe
+  "density", # Bevölkerungsdichte
+  "pctymale" # Anteil der jungen männlichen Bevölkerung
+  ) -> simpleModelColumns
+
+lm(
+  "crimes ~ .",
+  data = select(
+    data,
+    simpleModelColumns
+    )
+  ) -> simpleModel
+
+#With leaps
+leaps(
+  data.matrix(select(data,-c(crimes,region,name))),
+  data$crimes,
+  method="Cp",
+  nbest=5,
+  strictly.compatible = FALSE
+  ) -> leapsSelection
+
+which(leapsSelection$Cp == min(leapsSelection$Cp)) -> bestLeapsModel
+
+leapsSelection$label[leapsSelection$which[bestLeapsModel,]] -> bestLeapsLabels
 
 
-data[colnames(data)!="region" & colnames(data)!="name"] -> numericData
-cov(numericData) -> covNumericData
 
 
-sapply(10:nrow(data),function(x){
-  subset = covNumericData[sample(nrow(data),x),]
-  cov(subset) -> covSubset
-})
+
