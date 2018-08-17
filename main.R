8410fd6f3c85ee382f4f981f72564b3651f4d05e
Christian Lengert <clengert@googlemail.com>
2018-08-17 14:07:38 +0200

    add covariance accuracy stuff

--- main.R
+++ main.R
@@ -3,9 +3,30 @@ library(magrittr)
 library(ggplot2)
 library(purrr)
 library(leaps)
+set.seed(1)
 
 read.csv("dataset/crimes.csv") -> data
+select(data,-"name") -> dataWithoutName
 colnames(data) -> names
+
+numOfSamples = 15:nrow(dataWithoutName)
+
+lapply(numOfSamples,function(x){
+  dataWithoutName[sample(nrow(dataWithoutName),x),] -> s
+  cov(data.matrix(s))  
+}) -> covs
+
+sapply(covs[-length(covs)],function(x,best=covs[[length(covs)]]){
+  norm(best-x,type="2")
+}) -> dist
+
+data.frame(numOfSamples[-length(numOfSamples)],dist) -> distFrame
+c("count","dist") -> colnames(distFrame)
+
+lm("dist ~ count",distFrame) -> distModel
+print(distModel$coefficients)
+plot(distFrame$dist)
+abline(a=distModel$coefficients[1],b=distModel$coefficients[2])
 
 #Wählen Sie ein konkretes (einfaches) wahres Modell:
 c(
@@ -37,6 +58,16 @@ which(leapsSelection$Cp == min(leapsSelection$Cp)) -> bestLeapsModel
 leapsSelection$label[leapsSelection$which[bestLeapsModel,]] -> bestLeapsLabels
 
 
+
+
+
+lm(
+  "crimes ~ .",
+  data = select(
+    data,
+    c(bestLeapsLabels,'crimes')
+  )
+) -> bestLeapsModel
 
 
 
